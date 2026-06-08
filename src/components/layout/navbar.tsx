b05c813ae4517adcb095b22eda71b2a4bfce5c55
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-06-08 14:59:45 +0500

    feat: add mobile menu

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import clsx from 'clsx';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -32,69 +39,66 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="flex h-14 w-full items-center justify-between border-b border-gray-300 dark:border-gray-600">
-      <h1>
-        <Link
-          href="/"
-          className="hover:border-foreground border-b border-transparent text-xl font-semibold"
-        >
-          Muhammad Hanzla
-        </Link>
-      </h1>
-      <div className="flex items-center">
-        <ul className="mr-4 hidden gap-4 md:flex">
-          {links.map((link, idx) => (
-            <li key={idx}>
-              <Link
-                href={link.href}
-                className="rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900"
+    <nav className="relative">
+      <div className="flex h-14 w-full items-center justify-between border-b border-gray-300 dark:border-gray-600">
+        <h1>
+          <Link
+            href="/"
+            className="hover:border-foreground border-b border-transparent text-xl font-semibold"
+          >
+            Muhammad Hanzla
+          </Link>
+        </h1>
+        <div className="flex items-center">
+          <ul className="mr-4 hidden gap-4 md:flex">
+            {links.map((link, idx) => (
+              <li key={idx}>
+                <Link
+                  href={link.href}
+                  className="rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <button
+            className={clsx(
+              'mr-1 rounded p-2 hover:bg-gray-200 md:hidden dark:hover:bg-gray-900',
+              isOpen ? 'bg-gray-200 dark:bg-gray-900' : ''
+            )}
+            onClick={toggleMenu}
+            title="Menu"
+          >
+            {isOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
               >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <button
-          title="Menu"
-          className="rounded p-2 hover:bg-gray-200 md:hidden dark:hover:bg-gray-900"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
-        <button title="Search" className="rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </button>
-        <button
-          className="rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900"
-          onClick={toggleTheme}
-          title="Theme"
-        >
-          {!mounted ? null : resolvedTheme === 'light' ? (
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
+          <button title="Search" className="rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -106,27 +110,67 @@ export default function Navbar() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-              />
-            </svg>
-          )}
-        </button>
+          </button>
+          <button
+            className="ml-1 rounded p-2 hover:bg-gray-200 dark:hover:bg-gray-900"
+            onClick={toggleTheme}
+            title="Theme"
+          >
+            {!mounted ? null : resolvedTheme === 'dark' ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="bg-background top-14 left-0 z-100 absolute h-[calc(100vh-56px)] w-full">
+            <ul className="mt-24 flex flex-col items-center gap-1 space-y-8">
+              {links.map((link, idx) => (
+                <li key={idx}>
+                  <Link
+                    href={link.href}
+                    className="m-2 rounded p-4 text-lg hover:bg-gray-200 dark:hover:bg-gray-900"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
